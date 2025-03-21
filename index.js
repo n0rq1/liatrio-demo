ef004528b56a5ed13e967d6a31e893fe1d66f9e9
Austin <anorquist@mail.csuchico.edu>
2025-03-20 21:48:34 -0700

    fix: testing demo

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const PORT = 80;
 
 app.get('/', (req, res) => {
     const response = {
       message: "My name is Austin",
-      timestamp: Date.now()
+      timestamp: Date.now(),
+      Liatrio: "DevOps!!!!!!"
     };
     res.json(response);
 });
 
-app.listen(PORT = 80, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
